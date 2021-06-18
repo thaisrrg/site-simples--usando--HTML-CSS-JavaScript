ad95f324d416984c1c1a0432d40d07ecb2fe555a
Thais Roos Gularte <70456615+thaisrrg@users.noreply.github.com>
2021-06-18 01:32:35 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click',function(){
+    document.body.classList.toggle('dark-theme')
+
+    var ClassName = document.body.ClassName;
+    if(ClassName == "light-theme") {
+        this.textContent = "Dark";
+        
+    }
+    else{
+        this.textContent= "Light"
+    }
+});
